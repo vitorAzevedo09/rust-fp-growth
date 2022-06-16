c73119d3a37dea5ed9f74a9db237b45a0a3ddcbf
vitor <vitor.azevedo009@gmail.com>
2022-06-16 13:00:55 -0300

    Remaking code to read a .txt rather than .csv

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
 use clap::Parser;
-use csv::Reader;
-use std::error::Error;
-use std::fs::read_to_string;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
     #[clap(parse(from_os_str))]
-    input: std::path::PathBuf,
+    input: PathBuf,
     output: String,
-}
-
-fn csv_reader(output: String) -> Result<(), Box<dyn Error>> {
-    let mut rdr = Reader::from_path(output)?;
-    println!("{:?}", rdr);
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
 }
 
 fn main() {
     let args = Cli::parse();
-    let input = read_to_string(&args.input).expect("could not read file");
-    csv_reader(input).unwrap();
+    csv_reader(args.input).unwrap();
 }
