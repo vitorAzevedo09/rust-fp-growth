5b96505c9fbca742155ccaa9f237b160ea3cdc88
vitor <vitor.azevedo009@gmail.com>
2022-06-19 11:35:57 -0300

    Test some stuffs

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(parse(from_os_str))]
+    #[clap(short, long, parse(from_os_str))]
     input: PathBuf,
+    #[clap(short, long)]
     output: String,
+    #[clap(short, long)]
     minimun_sup: f64,
 }
 
@@ -16,9 +18,10 @@ struct Itemset {
     support: f64,
 }
 
-fn make_itemsets(transactions: String) -> HashMap<String, Itemset> {
+fn make_itemsets(dataset: String) -> (HashMap<String, Itemset>, f64) {
     let mut itemsets: HashMap<String, Itemset> = HashMap::new();
-    for row in transactions.split("\n") {
+    let mut len: f64 = 0.0;
+    for row in dataset.split("\n") {
         for item in row.split(",") {
             if item != "" {
                 let i = itemsets
@@ -27,18 +30,21 @@ fn make_itemsets(transactions: String) -> HashMap<String, Itemset> {
                 i.support += 1.0;
             }
         }
+        len += 1.0;
     }
-    itemsets
+    (itemsets, len)
 }
 
-fn filter_minimun_support(
+fn filter_itemsets(
     itemsets: HashMap<String, Itemset>,
+    len: f64,
     min_sup: f64,
 ) -> HashMap<String, Itemset> {
     let mut filtered_itemsets: HashMap<String, Itemset> = HashMap::new();
     for (hash, itemset) in itemsets {
-        if itemset.support >= min_sup {
-            filtered_itemsets.insert(hash, itemset);
+        let sup = itemset.support / len;
+        if sup >= min_sup {
+            filtered_itemsets.insert(hash, Itemset { support: sup });
         }
     }
     filtered_itemsets
@@ -50,13 +56,15 @@ fn main() {
     let transactions =
         fs::read_to_string(args.input).expect("Something went wrong reading the file");
 
-    let itemsets = filter_minimun_support(make_itemsets(transactions), args.minimun_sup);
+    let (itemsets, len) = make_itemsets(transactions);
+
+    let filtered_itemsets = filter_itemsets(itemsets, len, args.minimun_sup);
 
     let mut output = fs::File::create(args.output).unwrap();
 
-    for (hash, itemset) in &itemsets {
+    for (hash, i) in &filtered_itemsets {
         output
-            .write(format!("{} - {:?}\n", hash, itemset.support).as_bytes())
+            .write(format!("{} - {:?}\n", hash, i.support).as_bytes())
             .unwrap();
     }
 }
